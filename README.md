3ecc568c025513f82d61e962a4af08895e8aef1c
Sean Pines <spines83@gmail.com>
2014-04-29 22:20:14 -0400

    Adding in basic stuff for the message view

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ Clueless
 ========
 
 Game for Grad School
+
+To run:
+
+npm install
+node app.js
+
+Open 'http://localhost:3000' in a browser
---
+++ public/scripts/clueless/MessageView.js
@@ -0,0 +1,14 @@
+define([
+    'jquery',
+    'clueless/Communication'
+], function($, Communication){
+
+    var messageDiv = $("#messages");
+
+    var exposed = {
+
+    };
+
+    return exposed;
+
+});
