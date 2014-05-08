3d93725e5e27a0426396a557934663b13b27142e
Sean Pines <spines83@gmail.com>
2014-05-08 17:50:48 -0400

    adding in turn/move alerts

--- server/GameState.js
+++ server/GameState.js
@@ -2,6 +2,8 @@ var Communication = require('./Communication.js');
 var _ = require('underscore');
 
 var playerIds = [];
+var moveNo = 0;
+var cardsDealt = false;
 var playerToPiece = {};
 
 var nameToDisplay = {
@@ -13,7 +15,34 @@ var nameToDisplay = {
     white: "Mrs. White"
 }
 
-var cardsDealt = false;
+var roomToDisplay = {
+    study: "the Study",
+    studyLibraryHall: "the hallway between the Study and the Library",
+    library: "the Library",
+    libraryConservatoryHall: "the hallway between the Library and the Conservatory",
+    conservatory: "the Conservatory",
+    conservatoryBallRoomHall: "the hallway between the Conservatory and the Ballroom",
+    ballRoom: "the Ballroom",
+    ballRoomKitchenHall: "the hallway between the Ballroom and the Kitchen",
+    kitchen: "the Kitchen",
+    kitchenDiningRoomHall: "the hallway between the Kitchen and the Dining Room",
+    diningRoom: "the Dining Room",
+    diningRoomLoungeHall: "the hallway between the Dining Room and the Lounge",
+    lounge: "the Lounge",
+    loungeHallHall: "the hallway between the Lounge and the Hall",
+    hall: "the Hall",
+    studyHallHall: "the hallway between the Study and the Hall",
+    hallBilliardRoomHall: "the hallway between the Billiard Room and the Hall",
+    billiardRoom: "the Billiard Room",
+    diningRoomBilliardRoomHall: "the hallway between the Dining Room and the Billiard Room",
+    libraryBilliardRoomHall: "the hallway between the Library and the Billiard Room",
+    ballRoomBilliardRoomHall: "the hallway between the Ball Room and the Billiard Room",
+    loungeSecretPassage: "a secret passage in the Lounge",
+    conservatorySecretPassage: "a secret passage in the Conservatory",
+    studySecretPassage: "a secret passage in the Study",
+    kitchenSecretPassage: "a secret passage in the Kitchen"
+}
+
 
 exports.init = function(playerIdArray){
 
@@ -56,12 +85,31 @@ exports.init = function(playerIdArray){
                     card: card
                 });
             }
+
             cardsDealt = true;
         }
 
     });
 
     Communication.onMessageFromClient('player.move', function(sessionId, message){
+        if (cardsDealt){
+            moveNo = moveNo + 1;
+            var sessionId = playerIdArray[moveNo % playerIdArray.length];
+            console.log('move: ' + moveNo);
+            console.log('turn: ' + sessionId);
+
+            // Used by Message Panel
+            setTimeout(function(){
+                Communication.sendMessageToClientBySessionId(sessionId, 'panel.addMessage', {
+                    message: "--- It's your turn to move!! ---"
+                });
+            }, 250);
+        }
+
         Communication.sendMessageToClient('player.move', message);
+
+        Communication.sendMessageToClient('panel.addMessage', {
+            message: nameToDisplay[message.player] + " has moved to " + roomToDisplay[message.room]
+        })
     });
 }
